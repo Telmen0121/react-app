8b461bfef15af95f43b4af0254c743fd585fd6b7
B221930043 <telmen20050121@gmail.com>
2025-05-14 18:39:24 +0800

    header zasav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => (
         <div className="container header-content" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
             <div className="logo" style={{ fontSize: '1.7rem', display: 'flex', alignItems: 'center' }}>
                 <div className="logo-icon" style={{ fontSize: '2.5rem', marginRight: '0.5rem' }}>ISE</div>
-                International Stock Exchange
+                Хөрөнгийн бирж
             </div>
             <nav>
                 <ul style={{ display: 'flex', gap: '1.5rem', listStyle: 'none', margin: 0, padding: 0 }}>
-                    <li><Link to="/" style={{ fontSize: '1.2rem' }}>Home</Link></li>
-                    <li><Link to="/introduction" style={{ fontSize: '1.2rem' }}>Introduction</Link></li>
-                    <li><Link to="/market" style={{ fontSize: '1.2rem' }}>Market</Link></li>
-                    <li><Link to="/history" style={{ fontSize: '1.2rem' }}>History</Link></li>
-                    <li><Link to="/contact" style={{ fontSize: '1.2rem' }}>Contact</Link></li>
+                    <li><Link to="/" style={{ fontSize: '1.2rem' }}>Нүүр</Link></li>
+                    <li><Link to="/introduction" style={{ fontSize: '1.2rem' }}>Танилцуулга</Link></li>
+                    <li><Link to="/market" style={{ fontSize: '1.2rem' }}>Зах зээл</Link></li>
+                    <li><Link to="/history" style={{ fontSize: '1.2rem' }}>Түүх</Link></li>
+                    <li><Link to="/contact" style={{ fontSize: '1.2rem' }}>Холбоо барих</Link></li>
                 </ul>
             </nav>
         </div>
